af77531999c31ccb0fc01542c950df7c94036478
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2020-06-06 07:12:53 -0500

    corrigiendo error

--- controllers/grupoAlimenticio.js
+++ controllers/grupoAlimenticio.js
@@ -4,7 +4,7 @@ const con = require('../conexion/conexion');
 function getGrupoAlimenticios(req,res){
 
     con.query("SELECT * FROM Grupo_Alimenticio", function (err, sexo, field) {
-        if (err) return res.status(500).send({message:'Error al realizar la peticion'})
+        if (err) return res.status(500).send({message:err.message})
         res.status(200).send({data:sexo})
     });
 }
--- controllers/sexo.js
+++ controllers/sexo.js
@@ -4,7 +4,7 @@ const con = require('../conexion/conexion');
 function getSexos(req,res){
 
     con.query("SELECT * FROM Sexo", function (err, sexo, field) {
-        if (err) return res.status(500).send({message:'Error al realizar la peticion'})
+        if (err) return res.status(500).send({message:err.message})
         res.status(200).send({data:sexo})
     });
 }
