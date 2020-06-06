5ac9b3065df524764fe2b98bf40208781b2a56c1
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2020-06-06 07:18:47 -0500

    corrigiendo error

--- controllers/sexo.js
+++ controllers/sexo.js
@@ -3,7 +3,7 @@ const con = require('../conexion/conexion');
 
 function getSexos(req,res){
 
-    con.query("SELECT * FROM Sexo", function (err, sexo, field) {
+    con.query("SELECT * FROM sexo", function (err, sexo, field) {
         if (err) return res.status(500).send({message:err.message})
         res.status(200).send({data:sexo})
     });
