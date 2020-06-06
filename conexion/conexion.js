a90a3ef71b4e18a6435af0be72b74057701ea24a
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2020-06-06 07:39:29 -0500

    corrigiendo error

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost' ||'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user     : 'root' || 'ocg1lw0y9qgivy94',
-  password : '' || 'dgmnqp6wwem3ray1',
-  database : 'calculadora_nutricional' || 'r1tc3lb703nrg0lj'
+  host     : process.env.HOST || 'localhost' ,
+  user     : process.env.USER || 'root',
+  password : process.env.PASSWORD || '',
+  database : process.env.DB || 'calculadora_nutricional'
 });
 
 module.exports = connection;
