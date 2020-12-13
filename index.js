b6e16985a4f242a3e79c29a4b58ec64fc1af0c6c
majerhua123 <majerhua123@gmail.com>
2020-12-12 21:15:30 -0500

    prueba jenkins 6

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-  res.send("JAJAJA LO LOGRE");
+  res.send("JENKINS");
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
