2f7af9f19fb2dc3924eb648dc09605c57d99498e
majerhua123 <majerhua123@gmail.com>
2020-12-12 21:38:09 -0500

    prueba jenkins 9

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,14 +1,14 @@
-var mysql      = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-/*  host     : process.env.HOST || 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' ,
-  user     : process.env.USER || 'ocg1lw0y9qgivy94',
-  password : process.env.PASSWORD || 'dgmnqp6wwem3ray1',
-  database : process.env.DB || 'r1tc3lb703nrg0lj'*/
-
+  host: "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ocg1lw0y9qgivy94",
+  password: "dgmnqp6wwem3ray1",
+  database: "r1tc3lb703nrg0lj",
+  /*
   host     : process.env.HOST || 'localhost' ,
   user     : process.env.USER || 'root',
   password : process.env.PASSWORD || '',
-  database : process.env.DB || 'servicios_multiples'
+  database : process.env.DB || 'servicios_multiples'*/
 });
 
 module.exports = connection;
