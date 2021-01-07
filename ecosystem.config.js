c40fb2edbb1c09255ebb7d11735529f0323d3335
majerhua123 <majerhua123@gmail.com>
2021-01-07 07:25:11 -0500

    s

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "node-app",
-      script: "npm",
-      args: "start",
-    },
-  ],
-};
--- test/test.js
+++
@@ -1,7 +0,0 @@
-var request = require("supertest");
-var app = require("../index.js");
-describe("/", function () {
-  it("respond with hello world", function (done) {
-    request(app).get("/").expect("hello world", done);
-  });
-});
